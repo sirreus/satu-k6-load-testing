ac23dd4e6e5c2670feb40a322a03165c5f8b338e
Kirill Egorov <kirill.egorov@x5.ru>
2021-08-21 15:17:50 +0300

    cloud run test

--- tests/back.api.cart.js
+++ tests/back.api.cart.js
@@ -1,18 +1,10 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
+import { Rate } from "k6/metrics";
 
 const BASE_URL = "https://api-v2.satu.pro/api/front";
 
-export let options = {
-  stages: [
-    { duration: "5m", target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: "10m", target: 100 }, // stay at 100 users for 10 minutes
-    { duration: "5m", target: 0 }, // ramp-down to 0 users
-  ],
-  thresholds: {
-    http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
-  },
-};
+export let errorRate = new Rate("errors");
 
 export default function () {
   const endpoint = "/cart/get_goods";
@@ -23,9 +15,11 @@ export default function () {
   };
 
   let res = http.batch([req]);
-  check(res, {
+  const result = check(res, {
     "is status 200": (r) => r.status === 200,
   });
+
+  errorRate.add(!result);
 
   sleep(1);
 }
--- tests/back.api.products.js
+++ tests/back.api.products.js
@@ -3,17 +3,6 @@ import { check, sleep } from "k6";
 
 const BASE_URL = "https://api-v2.satu.pro/api/front";
 
-export let options = {
-  stages: [
-    { duration: "5m", target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: "10m", target: 100 }, // stay at 100 users for 10 minutes
-    { duration: "5m", target: 0 }, // ramp-down to 0 users
-  ],
-  thresholds: {
-    http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
-  },
-};
-
 export default function () {
   const endpoint = "/products/shtukaturnaya-stanciya-pft-g4-smart-bez-v-n-380v";
 
